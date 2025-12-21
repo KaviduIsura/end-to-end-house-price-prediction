616ec7d79f62f9d14181acebd0a6e57e768b029c
KaviduIsura <kaviduisura4567@gmail.com>
2025-12-21 10:46:30 +0530

    init frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="bg-red-600 text-white text-3xl p-6 rounded">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
+
